e8ee0d84c4d0363bd71e3365d56dd50a0a57b9eb
AlessandroChen <alex_passenger@outlook.com>
2019-05-31 17:16:55 +0800

    add templates

---
+++ templates/start.cpp
@@ -0,0 +1,13 @@
+// This code is for ACM competition
+// Author: AlessandroChen
+
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+#define ll long long
+#define rep(i, s, t) for(int i = (int)(s); i <= (int)(t); ++i)
+
+int main() {
+    return 0;
+}
---
+++ templates/start.py
@@ -0,0 +1,19 @@
+# Author: AlessandroChen
+# This Program is intended to
+
+# IMPORT SECTION 
+# from ... import ... as ...
+
+# CLASS SECTION
+
+# FUNCTIONS SECTION
+
+# MAIN PROGRAM
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
